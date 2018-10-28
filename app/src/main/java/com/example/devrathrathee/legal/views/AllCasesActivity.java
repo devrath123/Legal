e5803fcfe22a51b59fdffceb88a7625d05ad931c
Devrath <dev.rathee2010@gmail.com>
2018-10-28 23:49:04 +0530

    - All Cases created

---
+++ app/src/main/java/com/example/devrathrathee/legal/views/AllCasesActivity.java
@@ -0,0 +1,16 @@
+package com.example.devrathrathee.legal.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.devrathrathee.legal.R;
+
+public class AllCasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_cases);
+
+    }
+}
